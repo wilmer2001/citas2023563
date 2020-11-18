dfe84e1b7adbac3d1c12aceba47c91e8c1640c2b
wilmer <wilmeryesidrodriguez@gmail.com>
2020-11-18 18:47:38 -0500

    Integracion de vistas de las interfacces

---
+++ citas/JavaApplication9/src/modelo/GestorPacientes.java
@@ -0,0 +1,58 @@
+package modelo;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import recursos.conexion;
+
+public class GestorPacientes {
+    private static LinkedList<Paciente> pacientes;
+    Connection conn;
+    conexion conectar = new conexion();
+
+    public GestorPacientes() {
+        //recurso.Conexion conexion = new recursos.Conexion("localhost","root","","citas_medicas");    
+        //conn=cenexion.getConexion();
+    }
+
+    public void RegistrarPacientes(Paciente paciente) throws SQLException {
+
+        PreparedStatement pst;
+        try {
+            pst = conn.prepareStatement("insert into paciente values(?,?,?,?,?)");
+            pst.setString(1, paciente.getIdentificador());
+            pst.setString(2, paciente.getNombres());
+            pst.setString(3, paciente.getApellidos());
+            pst.setString(4, paciente.getFechaNacimiento());
+            pst.setString(5, paciente.getSexo());
+            JOptionPane.showMessageDialog(null, "Paciente Registrado");
+        } catch (SQLException ex) {
+            Logger.getLogger(GestorPacientes.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        //pacientes.add(paciente);
+    }
+}
+   /* public LinkedList<Paciente> getPacientebyParametro(int parametro, String valor) {
+        LinkedList<Paciente> resultado = new LinkedList<Paciente>();
+        String sql-"";
+//for(Paciente pac:pacientes)
+
+        switch(parametro){
+case 1: if(pac.getIdentificacion().equals(valor))
+resultado.add(pac);
+break;
+case 2: if(pac.getNombres().equals(valor))
+resultado.add(pac);
+break;
+case 3: if(pac.getApellidos().equals(valor))
+resultado.add(pac);
+break;
+case 4: if(pac.getGenero().equals(valor))
+resultado.add(pac);
+break;
+         */
